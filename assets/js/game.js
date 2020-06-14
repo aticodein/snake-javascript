e84833c3b47f5971153c8cfb7db6721e6167fbfc
Attila Szaloki <szalokiattila@gmail.com>
2020-06-14 15:07:38 +0000

    works with no explanation

--- assets/js/game.js
+++ assets/js/game.js
@@ -1,27 +1,46 @@
-import { update as updateSnake, draw as drawSnake, SNAKE_SPEED } from "/assets/js/snake.js"
+import { update as updateSnake, draw as drawSnake, SNAKE_SPEED, getSnakeHead, snakeIntersection } from "/assets/js/snake.js";
+import { update as updateFood, draw as drawFood } from "/assets/js/food.js";
+import { outsideGrid } from "/assets/js/grid.js"
 
 
 let lastRenderTime = 0
-const gameBoard = document.getElementById("game-board")
+let gameOver = false
+const gameBoard = document.getElementById('game-board')
 
 function main(currentTime) {
-    window.requestAnimationFrame(main)
-    const secondsSinceLastRender = (currentTime - lastRenderTime) / 1000
-    if (secondsSinceLastRender < 1 / SNAKE_SPEED) return
+  if (gameOver) {
+    if (confirm('You lost. Press ok to restart.')) {
+      window.location = '/'
+    }
+    return
+  }
 
-    lastRenderTime = currentTime
 
-    update()
-    draw()
-   
+  window.requestAnimationFrame(main)
+  const secondsSinceLastRender = (currentTime - lastRenderTime) / 1000
+  if (secondsSinceLastRender < 1 / SNAKE_SPEED) return
+
+
+  lastRenderTime = currentTime
+
+  update()
+  draw()
 }
 
 window.requestAnimationFrame(main)
 
 function update() {
   updateSnake()
+  updateFood()
+  checkDeath()
 }
 
 function draw() {
+  gameBoard.innerHTML = ''
   drawSnake(gameBoard)
+  drawFood(gameBoard)
+}
+
+function checkDeath() {
+  gameOver = outsideGrid(getSnakeHead()) || snakeIntersection()
 }
